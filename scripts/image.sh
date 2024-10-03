4d5e42d509b2d0a959fb81ac8d4d5ccb748b40b8
David Elie-Dit-Cosaque <deliedit@redhat.com>
2024-10-03 18:05:24 -0400

    fix lint

--- scripts/image.sh
+++ scripts/image.sh
@@ -7,5 +7,5 @@ make test
 go install github.com/karalabe/xgo@latest
 xgo --targets=linux/arm64,linux/amd64 .
 podman manifest create ${REPO}/${IMAGE_NAME}:${VERSION}
-podman build --platform linux/amd64,linux/arm64  --manifest ${REPO}/${IMAGE_NAME}:${VERSION} --rm -f Dockerfile  .
+podman build --platform linux/amd64,linux/arm64 --manifest ${REPO}/${IMAGE_NAME}:${VERSION} --rm -f Dockerfile .
 podman manifest push ${REPO}/${IMAGE_NAME}:${VERSION}
