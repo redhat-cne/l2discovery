3d017f35886eccdc7cd342384f40dae8bdb320eb
David Elie-Dit-Cosaque <deliedit@redhat.com>
2024-10-03 17:58:59 -0400

    try xgo github action

--- scripts/image.sh
+++ scripts/image.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -x
-VERSION=latest
+VERSION=multi
 IMAGE_NAME=l2discovery
 REPO=quay.io/redhat-cne
 make test
-go build l2discovery.go
-podman build -t ${IMAGE_NAME} --rm -f Dockerfile .
-podman tag ${IMAGE_NAME} ${REPO}/${IMAGE_NAME}:${VERSION}
-podman push ${REPO}/${IMAGE_NAME}:${VERSION}
+go install github.com/karalabe/xgo@latest
+xgo --targets=linux/arm64,linux/amd64 .
+podman manifest create ${REPO}/${IMAGE_NAME}:${VERSION}
+podman build --platform linux/amd64,linux/arm64  --manifest ${REPO}/${IMAGE_NAME}:${VERSION} --rm -f Dockerfile  .
+podman manifest push ${REPO}/${IMAGE_NAME}:${VERSION}
