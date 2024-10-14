8b6987fcafb6386db6ca39af400cb5c61dfce13b
David Elie-Dit-Cosaque <deliedit@redhat.com>
2024-10-14 13:25:37 -0400

    try github.com/crazy-max/xgo@latest xgo

--- scripts/image.sh
+++ scripts/image.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
-set -x
+#!/usr/bin/env bash
 VERSION=latest
 IMAGE_NAME=l2discovery
 REPO=quay.io/redhat-cne
 make test
-go install github.com/karalabe/xgo@latest
-xgo --targets=linux/arm64,linux/amd64 .
+
+if [[ $# -eq 0 ]]; then
+	echo "Running locally"
+	go install github.com/crazy-max/xgo@latest
+	mkdir -p /tmp/xgo-cache
+	xgo --targets=linux/arm64,linux/amd64 .
+fi
 podman manifest create ${REPO}/${IMAGE_NAME}:${VERSION}
 podman build --platform linux/amd64,linux/arm64 --manifest ${REPO}/${IMAGE_NAME}:${VERSION} --rm -f Dockerfile .
 podman manifest push ${REPO}/${IMAGE_NAME}:${VERSION}
